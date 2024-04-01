f38f8d1ad16bd693a1fc0dc035906df2ac8c4895
Anton Savin <a.nivas@yandex.ru>
2024-04-01 19:35:32 +0400

    upd gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -10,28 +10,18 @@ const expressionValue = () => {
 };
 
 const rightAnswer = (expValue) => {
-  const firstNumDividers = [];
-  const secondNumDividers = [];
-  const commonDividers = [];
-  const arr = expValue.split(' ');
-  const firstNum = Number(arr[0]);
-  const secondNum = Number(arr[1]);
-  for (let i = 1; i <= firstNum; i += 1) {
-    if (firstNum % i === 0) {
-      firstNumDividers.push(i);
-    }
-  }
-  for (let j = 1; j <= secondNum; j += 1) {
-    if (secondNum % j === 0) {
-      secondNumDividers.push(j);
-    }
-  }
-  for (const index1 of firstNumDividers) {
-    if (secondNumDividers.includes(index1)) {
-      commonDividers.push(index1);
-    }
-  }
   let maxDivider = 1;
+  const arrayOfNumers = expValue.split(' ');
+  const [firstNum, secondNum] = arrayOfNumers;
+  if (firstNum === 0 || secondNum === 0) {
+    return maxDivider.toString();
+  }
+  const commonDividers = [];
+  for (let i = 1; i <= firstNum; i += 1) {
+    if (firstNum % i === 0 && secondNum % i === 0) {
+      commonDividers.push(i);
+    }
+  }
   for (const divider of commonDividers) {
     if (divider > maxDivider) {
       maxDivider = divider;
