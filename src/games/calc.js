94e5d8f7c45da3c98233e7cf956d8830892e57aa
Anton Savin <a.nivas@yandex.ru>
2024-04-02 10:38:18 +0400

    upd

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,5 @@
 import checkUserAnswer from '../index.js';
-import{ getRandomNumber } from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const askUser = () => {
   const mathRiddle = 'What is the result of the expression?';
--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,5 @@
 import checkUserAnswer from '../index.js';
-import { getRandomNumber } from '../utils.js';
-import { checkEven } from '../utils.js';
+import { getRandomNumber, checkEven } from '../utils.js';
 
 const askUser = () => {
   const mathRiddle = 'Answer "yes" if the number is even, otherwise answer "no".';
